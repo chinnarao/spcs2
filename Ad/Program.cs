d8cad6e1d862a4a352a65a8ab3f73e693b307d43
chinnarao <chinnarao@live.com>
2018-10-31 13:47:26 -0700

    no change

--- Ad/Program.cs
+++ Ad/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Logging;
-
+// https://stackoverflow.com/questions/46395192/is-specifying-the-listening-http-port-via-useurls-the-correct-way
 namespace Ad
 {
     public class Program
